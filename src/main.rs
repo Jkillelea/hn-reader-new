5e85c81549ce142c30ec25a9860c0516f62b1f9e
Jacob Killelea <jkillelea@protonmail.ch>
2018-05-21 22:41:45 -0700

    misc

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,14 @@ use std::thread;
 
 const BASE_URL: &str = "https://hacker-news.firebaseio.com/";
 
+#[derive(Debug)]
+enum Article {
+    Text(String), // the actual article
+    Waiting,      // waiting on IO,
+}
+
 // ArticleBuffer struct holds a list of articles and allows a user to iterate over them
+#[derive(Debug)]
 struct ArticleBuffer {
     buffer: Arc<Mutex<Vec<String>>>,
     thread_running: Arc<Mutex<bool>>,
@@ -25,20 +32,17 @@ impl ArticleBuffer {
 
         let buf = article_buffer.buffer.clone(); // cloning these only clones a *handle*
         let running = article_buffer.thread_running.clone();
-        thread::spawn(move || {
+        thread::spawn(move || { // Launch a new thread
             let base   = Url::parse(BASE_URL).unwrap();
             let client = reqwest::Client::new();
-            println!("fetching {:?}", ids);
-            for i in ids {
-                println!("fetching {}", i);
+            for id in ids { // for each article ID
                 let mut body = String::new();
-                let url = base.join(&format!("/v0/item/{}.json?print=pretty", i)).unwrap();
-                let mut response = client.get(url).send().unwrap();
-
-                response.read_to_string(&mut body).unwrap();
-                buf.lock().unwrap().push(body);
+                let url = base.join(&format!("/v0/item/{}.json?print=pretty", id)).unwrap(); // Generate a URL
+                let mut response = client.get(url).send().unwrap(); // make the request
+                response.read_to_string(&mut body).unwrap();        // read it in
+                buf.lock().unwrap().push(body);                     // and add it to the list
             }
-            *running.lock().unwrap() = false;
+            *running.lock().unwrap() = false; // When done, let the main thread know
         });
 
         article_buffer
@@ -46,19 +50,17 @@ impl ArticleBuffer {
 }
 
 impl Iterator for ArticleBuffer { // Loop over the content
-    type Item = String; // The type of item we'll return is a string
+    type Item = Article; // The type of item we'll return is a string
 
     // The only required method. Each time it is called, it should either return
     // the next item that we want, or None.
     fn next(&mut self) -> Option<Self::Item> {
         let article = self.buffer.lock().unwrap().pop();
-        if article.is_some() { // See if an article is ready
-            article            // and if so, return it
+        if article.is_some() {                    // See if an article is ready
+            Some(Article::Text(article.unwrap())) // and if so, return it
         } else {
-            if *self.thread_running.lock().unwrap() { // If we're waiting on network,
-                println!("Waiting");                  // we sleep a little
-                thread::sleep(::std::time::Duration::from_millis(100));
-                return self.next() // call this function again to check again
+            if *self.thread_running.lock().unwrap() { // If we're waiting on network
+                return Some(Article::Waiting);
             }
             None // Actually out of content? Return None
         }
@@ -66,19 +68,25 @@ impl Iterator for ArticleBuffer { // Loop over the content
 }
 
 fn main() -> Result<(), Box<::std::error::Error>> {
-    let base = Url::parse(BASE_URL)?;
-    let top = base.join("/v0/topstories.json")?;
-    let client = reqwest::Client::new();
+    let     base     = Url::parse(BASE_URL)?;
+    let     top      = base.join("/v0/topstories.json")?;
+    let     client   = reqwest::Client::new();
     let mut response = client.get(top).send()?;
-    let mut body = String::new();
+    let mut body     = String::new();
     response.read_to_string(&mut body)?;
     let vals = char_delimited_string_to_array(&body).unwrap();
 
     let mut article_buffer = ArticleBuffer::new(vals);
-    thread::sleep(::std::time::Duration::from_secs(10));
+    thread::sleep(::std::time::Duration::from_secs(1));
 
     while let Some(art) = article_buffer.next() {
-        println!("{}", art);
+        match art {
+            Article::Text(text) => println!("{}", text),
+            Article::Waiting => {
+                println!("Waiting");
+                thread::sleep(::std::time::Duration::from_millis(100));
+            },
+        }
     }
 
     Ok(())
@@ -89,17 +97,13 @@ fn char_delimited_string_to_array(string: &String) -> Result<Vec<i32>, Box<::std
     let vals: Vec<i32> = string.split(",").map(|x| {
         x.parse().unwrap_or_else(|_| {
             if x.clone().chars().next().unwrap().is_numeric() { // last char is non-numeric
-                // println!("x 0 numeric: {}", x);
                 let n = &x[0 .. x.len()-1]; // everything except last char
-                // println!("truncated: {}", n);
                 n.parse().unwrap()
             } else if x.clone().chars().last().unwrap().is_numeric() { // first char is non-numeric
-                // println!("x last numeric: {}", x);
                 let n = &x[1 .. x.len()]; // everything except first char
-                // println!("truncated: {}", n);
                 n.parse().unwrap()
             } else {
-                99999999
+                9999999
             }
         })
     }).collect();
